a43a293bc8f1f7f424111c756181bd49b1a754f6
elkonele <elkonele@gmail.com>
2021-11-27 19:28:28 +0300

    2

--- main.go
+++ main.go
@@ -2,6 +2,19 @@ package main
 
 import "fmt"
 
+type Person struct {
+	Id      int
+	Name    string
+	Address string
+}
+type Account struct {
+	Id int
+	//Name    string
+	Cleaner func(string) string
+	Owner   Person
+	Person
+}
+
 func main() {
 	var a int = 25
 	fmt.Println(a)
